586ef8a947283bc8e7cafbe6a4dafb2f0899f1c3
Shuai Yuan <ms.ysboss@gmail.com>
2021-03-24 23:45:50 -0700

    Add comments
    Add comments for the method process() and saveImage()

--- method.py
+++ method.py
@@ -3,13 +3,25 @@ import cv2
 
 def process(image, lowGray, highGray):
     img = cv2.imread(image)
+    
+    # Cuts off irrelevant part, only keep the tube.
     img = img[1950:2100, :5200]
     height, width, c = img.shape
+    
+    # Converts the image from RGB to grayscale.
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    
+    # Stores the average pixel value of each column. 
     means = []
+    
+    # Stores the diff of the average pixel value of left side [0, x-1] and 
+    # the average pixel value of right side [x+1, n] at the position x. 
+    #
+    # Example: for a 5 column image, means = [10,18,30,50,70], at the position 2
+    # (means[2] = 30), the diff = (50 + 70) / 2 - (10 + 18) / 2 = 46.
     diffs = []
 
+    # Calculates the average pixel value for each column. 
     for i in range(width):
         values = []
         for j in range(height):
@@ -18,6 +30,8 @@ def process(image, lowGray, highGray):
                 values.append(value)
         means.append(int(np.mean(values)))
 
+    # Calculates the diff of the average pixel value of left side [0, x-1] and 
+    # the average pixel value of right side [x+1, n] at the position x.
     for index in range(1, len(means)):
         left = []
         right = []
@@ -27,30 +41,44 @@ def process(image, lowGray, highGray):
             right.append(means[j])
         diffs.append(int(np.mean(right)) - int(np.mean(left)))
 
+    # Gets the largest diff value.
     largest = max(diffs)
+    
+    # Gets the frist index of the largest diff value in diffs. 
     total = diffs.index(largest)
+    
+    # The number of the largest diff value.
     amount = 1
+    
+    # Calculates how many columns have the largest diff value in a 30 width. 
+    # (30 is a reasonable range cause the width of the water interface is less 
+    #  than 30 typically)
     for i in range(diffs.index(largest), diffs.index(largest) + 30):
+        # Finds a column has the largest diff value, adds its index to 'total' 
+        # increments 'amount' by 1.
         if(diffs[i] == largest):
             total+=i
             amount+=1
 
+    # Returns the image and the index of the most possible water interface.
     return (image, int(total/amount))
 
-# def write2file(imgIndex, pos):
-#     with open('result.txt', 'w') as fd:
-#         line = imgIndex + ', ' + pos + '\n'
-#         fd.write
-
-
+# Draws a red line at the column 'pos' and saves the image.
+# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_drawing_functions/py_drawing_functions.html
 def saveImage(image, pos):
     img = cv2.imread(image)
     height, width, c = img.shape
+   
+    # Draws a red(0,0,255) line from (pos, 0) to (pos, height) with thickness of 5 px.
     img = cv2.line(img, (pos,0), (pos,height), (0,0,255), 5)
     cv2.imwrite('images_final/'+image[16:], img)
     #cv2.imshow('new', img)
     #cv2.waitKey()
 
+
+#############################################################################
+#############################################################################
+#############################################################################
 
 # img = cv2.imread('settling_column/MFDC6201.JPG')
 # #img = cv2.resize(img, None, None, 0.3, 0.3)
